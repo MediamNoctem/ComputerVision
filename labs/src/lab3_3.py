093b2f08adaa934e513c142d09db5f12cde324a9
Anastasia <romAnastasia79@yandex.ru>
2024-10-21 21:38:29 +0300

    Update 3

--- labs/src/lab3_3.py
+++ labs/src/lab3_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class Points:
@@ -46,11 +47,11 @@ class Points:
             return None, None, None, None
 
         r_min = min([p[0] for p in self.points])
-        c_min = min([p[1] for p in self.points])
         r_max = max([p[0] for p in self.points])
+        c_min = min([p[1] for p in self.points])
         c_max = max([p[1] for p in self.points])
 
-        return r_min, c_min, r_max, c_max
+        return r_min, r_max, c_min, c_max
 
     def get_principal_axes(self):
         """
@@ -80,7 +81,7 @@ points.add_point(1, 2)
 points.add_point(3, 4)
 points.add_point(5, 6)
 
-print("Точки:")
+print("Точки в формате [y, x]:")
 
 for i in range(len(points.points)):
     print(points.points[i])
@@ -94,10 +95,35 @@ for key, value in moments.items():
     print(f"{key}: {value}")
 
 r_min, r_max, c_min, c_max = points.get_bounding_box()
-print(f"Описывающий прямоугольник: (({r_min}, {c_min}), ({r_max}, {c_max}))")
+print(f"Описывающий прямоугольник: (({r_max}, {c_min}), ({r_min}, {c_max}))")
 
 axes = points.get_principal_axes()
 print("Главные оси:")
 print(f"Угол в градусах: {(axes['angle'] * 180) / 3.1415}")
 print(f"Момент второго порядка относительно r: {axes['mrr']}")
 print(f"Момент второго порядка относительно c: {axes['mcc']}")
+
+x = [points.points[i][1] for i in range(len(points.points))]
+y = [points.points[i][0] for i in range(len(points.points))]
+
+if axes['angle'] < 90:
+    x2 = c_max
+    y2 = 0 + (x2 - 0) * np.tan(axes['angle'])
+elif axes['angle'] > 90:
+    y2 = r_max
+    x2 = 0 + (y2 - 0) / np.tan(axes['angle'])
+else:
+    x2 = 0
+    y2 = r_max
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, c_max + 1)
+ax.set_ylim(0, r_max + 1)
+plt.plot(x, y, 'ro', label='Точки')
+ax.plot([0, x2], [0, y2], 'black', linestyle='--', label='Главная ось')
+rect = plt.Rectangle((c_min, r_max), c_max - c_min, r_min - r_max, fill=False, color='blue', label='Описывающий прямоугольник')
+ax.add_patch(rect)
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
